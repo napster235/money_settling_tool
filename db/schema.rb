0731647691fa8b227bd329fd6ad2bd0b5d8d7d32
Tudor <tudor.dragomir@sparktech.ro>
2020-05-05 13:00:42 +0300

    added postgresql db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_02_04_145233) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bills", force: :cascade do |t|
     t.string "name", null: false
     t.integer "total_amount", null: false
